84989a662bc5333f325e59a0a7f7a43fefdb03ca
jpacombiac <jpacombiac@unjbg.edu.pe>
2021-02-18 22:12:48 -0500

    Update Producto.cpp

--- Producto.cpp
+++ Producto.cpp
@@ -8,22 +8,4 @@ Producto :: Producto(string n, int s){
 	hi;
 	cliente::cliente(string p , int v)
 	
-	#include<iostream>
-
-using namespace std;
-
-int cubo(int);
-
-int main(){
-	int n;
-	cout<<"\n Ingrese un numero para calcular el cubo:";
-	cin>>n;
-	cout<<"\nEl cubo de "<< n <<" es : "<< cubo(n) <<endl;
-	
-	return 0;
-		
-}
-int cubo(int c){
-	return c * c * c;
-}
 }
