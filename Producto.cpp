6a1333780fbfa4759ba8964032a57a63f3816328
jpacombiac <jpacombiac@unjbg.edu.pe>
2021-02-25 11:37:57 -0500

    Update Producto.cpp

--- Producto.cpp
+++ Producto.cpp
@@ -4,8 +4,11 @@ Producto :: Producto(string n, int s){
 	nombre = n;
 	stock = s;
 	
+	
+Conta :: Conta(string h, int j)
 	hola mundo;
 	hi;
+<<<<<<< HEAD
 <<<<<<< HEAD
 	cliente::cliente(string p , int v)
 <<<<<<< HEAD
@@ -18,3 +21,10 @@ Producto :: Producto(string n, int s){
 =======
 }
 >>>>>>> parent of 44042b0 (Update Producto.cpp)
+=======
+
+	cout<<"hola a todos";
+
+	cliente::cliente(string p , int v)
+}
+>>>>>>> 83f5b0625121b0dd9ac8df6671b05ca65e7c712b
