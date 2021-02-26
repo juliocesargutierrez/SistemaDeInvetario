b4bc76ea150d98b27e633000ea5c3ff074b8beff
jpacombiac <jpacombiac@unjbg.edu.pe>
2021-02-25 22:38:45 -0500

    implementacion bodega

--- Bodega.cpp
+++ Bodega.cpp
@@ -1,1 +1,5 @@
 #include "Bodega.h"
+
+using namespace std;
+
+Bodega::Bodega(string )
--- Bodega.h
+++ Bodega.h
@@ -1,10 +1,23 @@
 #ifndef BODEGA_H
 #define BODEGA_H
 
+#include "Bogeda.h"
+
+using namespace std;
+
 class Bodega
 {
+	private :
+		char nombre[20];
+		char direccion[20];
+		int telefono;
+		char ciudad[20];
+		
 	public:
-	protected:
+		void mostrardatos();
+		void registrardatos();
+		void cambiardatos();
+		
 };
 
 #endif
