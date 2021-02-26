956d654675c6395e2a6ed4811cf1cf98ccfa00c6
juliocesargutierrez <jcgutierrezcutipa@gmail.com>
2021-02-25 22:23:52 -0500

    implementando

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "Producto.h"
+#include "Venta.h"
+#include "Proveedor.h"
+
+using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
